9ee48a6d7f6d97ec9a9ab943188646a684e0f1dd
Chris Adams <chris.adams@jollamobile.com>
2019-11-07 11:02:08 +1000

    Update various license headers

--- src/sqlitestorage.h
+++ src/sqlitestorage.h
@@ -2,7 +2,8 @@
   This file is part of the mkcal library.
 
   Copyright (c) 2009 Nokia Corporation and/or its subsidiary(-ies). All rights reserved.
-  Contact: Alvaro Manera <alvaro.manera@nokia.com>
+  Copyright (c) 2014-2019 Jolla Ltd.
+  Copyright (c) 2019 Open Mobile Platform LLC.
 
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Library General Public
