b4cf0bb2839c15b5bd5d1515064ab21f7943f7f3
Damien Caliste <dcaliste@free.fr>
2023-01-25 14:00:40 +0100

    [mkcal] Fix discrepency between migrated and new DBs. Contributes to JB#59982
    The INSERT and UPDATE sharing the same code in
    sqliteformat.cpp, and INSERT relying on column
    order, new column should be added at the end in
    the CREATE statement. Otherwise, there is a
    difference between migrated DBs where the new
    column is at the end and new DBs where the column
    has been added in the middle of other columns.
    
    Correct this issue for the new thisAndFuture column.

--- src/sqliteformat.cpp
+++ src/sqliteformat.cpp
@@ -621,10 +621,11 @@ bool SqliteFormat::modifyComponents(const Incidence::Ptr &incidence, const QStri
         }
         SL3_bind_int(stmt1, index, percentComplete);
         SL3_bind_date_time(this, stmt1, index, effectiveDtCompleted, incidence->allDay());
-        SL3_bind_int(stmt1, index, incidence->thisAndFuture());
 
         colorstr = incidence->color().toUtf8();
         SL3_bind_text(stmt1, index, colorstr.constData(), colorstr.length(), SQLITE_STATIC);
+
+        SL3_bind_int(stmt1, index, incidence->thisAndFuture());
 
         if (dbop == DBUpdate)
             SL3_bind_int(stmt1, index, rowid);
@@ -1595,12 +1596,14 @@ Incidence::Ptr SqliteFormat::selectComponents(sqlite3_stmt *stmt1, QString &note
 
         index++; //DateDeleted
 
-        incidence->setThisAndFuture(sqlite3_column_int(stmt1, index++));
-
         QString colorstr = QString::fromUtf8((const char *) sqlite3_column_text(stmt1, index++));
         if (!colorstr.isEmpty()) {
             incidence->setColor(colorstr);
         }
+
+        index++; // extra2
+        index++; // extra3
+        incidence->setThisAndFuture(sqlite3_column_int(stmt1, index++));
 //    kDebug() << "loaded component for incidence" << incidence->uid() << "notebook" << notebook;
 
         if (!d->selectCustomproperties(incidence, rowid)) {
